cba1350156a3c8edefac8769568d7b45b464ee6b
sowmyasri-Z <zsowmyasri.2402@gmail.com>
2023-10-18 10:33:24 +0530

    Created a Python solution for RockPaperScissor #1

--- RPS2.py
+++ RPS2.py
@@ -1,5 +1,5 @@
 import random
-
+#This line imports the random module, which is used to generate a random choice for the computer.
 def get_player_choice():
     while True:
         player = input("Enter your choice (rock, paper, scissors): ").lower()
@@ -7,10 +7,10 @@ def get_player_choice():
             return player
         else:
             print("Invalid input. Please choose from rock, paper, or scissors.")
-
+#this fuction allows player to enter their choice using while loop from rock,paper and scissors
 def get_computer_choice():
     return random.choice(["rock", "paper", "scissors"])
-
+#this function generates a random choice from computer
 def determine_winner(player, computer):
     outcomes = {
         ("rock", "paper"): "Computer wins",
@@ -23,7 +23,7 @@ def determine_winner(player, computer):
     if player == computer:
         return "Tie"
     return outcomes.get((player, computer), "Invalid input")
-
+# all the possible cases from player and the system
 def main():
     player = get_player_choice()
     computer = get_computer_choice()
@@ -32,6 +32,6 @@ def main():
     print(f"Player chose {player}")
     print(f"Computer chose {computer}")
     print(result)
-
+#this function determines the winner
 if __name__ == "__main__":
     main()
